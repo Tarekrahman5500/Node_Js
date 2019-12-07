8e1de91d28d4777b30f0aa3abf8ebcfa1543fefe
Tarek Rahman <tr617739@gmail.com>
2019-12-07 12:27:33 +0600

    About middleware

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const  app = express();
+
 app.get('/',(req,res) =>
 {
     res.send("We are Start again");
+
+});
+app.get('/posts',(req,res) =>
+{
+    res.send("We are posts again");
 
 });
 app.listen(300);
